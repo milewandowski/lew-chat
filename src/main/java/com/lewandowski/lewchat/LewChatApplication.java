fe96257767c7ce59427a9b0d10fc1332f34577fc
Michal Lewandowski <mlewandowskimichal@gmail.com>
2021-04-16 18:18:25 +0200

    Initial commit

---
+++ src/main/java/com/lewandowski/lewchat/LewChatApplication.java
@@ -0,0 +1,13 @@
+package com.lewandowski.lewchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LewChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LewChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lewandowski/lewchat/LewChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lewandowski.lewchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LewChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
